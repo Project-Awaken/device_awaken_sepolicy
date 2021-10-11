7cd250dcd010e3c32595b5f8babed53c29903a54
PainKiller3 <ninadpatil100@gmail.com>
2021-10-11 13:50:40 +0530

    sepolicy: Adapt path for awakenOS
    Change-Id: I734f370d1a0db02b0e674de9efb6e5ae443fb530

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from Awaken
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/awaken/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/awaken/sepolicy/atv/private
